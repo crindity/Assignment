4223779df93ebe22963cfb9ef7cfe8d1cea14285
crindity <166424691+crindity@users.noreply.github.com>
2024-04-21 20:37:10 +0600

    Assignment

---
+++ assignment.js
@@ -0,0 +1,68 @@
+// ১. Variable কি ?
+
+//Ans: বেলুয়েবল হলো তথ্যকে স্টোর করে রাখা এবং প্রয়োজন অনুযায়ী সেটা ব্যবহার করা, 
+//তথ্যকে আপডেট করা এবং তথ্যকে পরিবর্তন করতে পারা
+
+// ২. Variable কিভাবে লিখতে হয় ?
+
+//Ans: var friendsName = "Jaber";
+//     console.log(friendsName)
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+
+//Ans: var strType ="Jaber Ahmed";
+    //   console.log(typeof strType)
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+
+//Ans:   var numType = 50;
+//       console.log(numType)
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+
+//Ans:var BoolType = True;
+//    console.log(BoolType)
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ? 
+
+//Ans: var lorem = "Loremipsum50word text here";
+//    console.log(lorem .toUpperCase())
+//    var lorem = "Loremipsum50word text here";
+//    console.log(lorem .toLowerCase())
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+
+//Ans: অপারেটর হল বিভিন্ন গাণিতিক অপারেসন করার জন্য ব্যবহৃত বিভিন্ন চিহ্ন। 
+//     যেমন - যোগ, বিয়োগ, গুন, ভাগ ইত্যাদি হল গাণিতিক অপারেসন।
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন । 
+
+//Ans: ধনাত্মক সংখ্যাকে ধনাত্মক করতে ব্যবহৃত হয়
+//     var sum = math abs (num1)
+//     console.log(sum)
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন । 
+
+//Ans: সংখ্যা 0.1থাকলেও  1 এ যাবে এবং 0.99  থাকলেও 1 এ যাবে
+//     var sum = math .ceil(voter)
+//     console.log(sum)
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন ।
+
+//Ans: 0.99 থাকলে ও সংখ্যা 1 না হয়ে 0 হবে
+//     var sum = math .floor(voter)
+//     console.log(sum)
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+
+//Ans: সংখ্যাকে পয়েন্টে থাকলে বাড়িয়ে দিতে যেমন 0.5 থাকলে 1 হবে
+//     var sum = math .round(voter)
+//     console.log(sum)
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+
+//Ans: একেক সংখ্যা একেকবার Show করাত ব্যবহৃত হয়
+//     var lotary = math .random()*100;
+//     console.log(lotary)
+
+// asignment.js - GitHub Link 
